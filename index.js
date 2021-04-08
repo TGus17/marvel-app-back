1d69f8b07818d439cb606fb3d9fc6eb89c68f0fa
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-04-07 22:02:30 -0300

    :construction: Creating middlewares at Register route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const RegisterController = require('./controllers/RegisterController');
+const { validateDatas, validateNewUser } = require('./middlewares/validations');
 
 const app = express()
 const port = 3000
@@ -8,7 +9,6 @@ const port = 3000
 app.use(cors());
 app.use(express.json());
 
-app.use('/register', RegisterController);
+app.use('/register', validateDatas, validateNewUser, RegisterController);
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
