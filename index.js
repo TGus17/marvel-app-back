c9259885e8fa2e2c61ebf0e0ca026754c4db17c5
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-04-08 09:30:39 -0300

    :bug: Changing the port to listen

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const { LoginController, RegisterController } = require('./controllers');
 const { validateDatas, validateName, validateNewUser } = require('./middlewares/validations');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -12,4 +12,4 @@ app.use(express.json());
 app.use('/register', validateDatas, validateName, validateNewUser, RegisterController);
 app.use('/login', validateDatas, LoginController);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
