02f36a021963db964219e7fdc8739c85f95b843b
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-04-09 20:08:33 -0300

    :recycle: refactored register route without token

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,13 +1,11 @@
 const { Router } = require('express');
 const service = require('../services/UserService');
-const deliverToken = require('../util/deliverToken');
 const RegisterController = new Router();
 
 RegisterController.post('/', async (req, res) => {
   const { name, email, password } = req.body;
   const user = await service.createUser({ name, email, password });
-  const token = deliverToken(user);
-  res.status(201).json({ token });
+  res.status(201).json(user);
 });
 
 module.exports = RegisterController;
